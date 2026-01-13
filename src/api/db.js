c620d2d4d75648e6518823998c54a43ee9b196e5
istmtunde <ist.m.tunde@gmail.com>
2026-01-13 21:26:06 +0100

    port config

--- src/api/db.js
+++ src/api/db.js
@@ -2,6 +2,7 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT), // Port konfiguráció
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
